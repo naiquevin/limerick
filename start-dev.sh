8d7a20a705e4f5a75d0712c0f6cbfc23c0fc9919
Vineet <naikvin@gmail.com>
2013-03-20 22:48:29 +0530

    Change -name option to -sname in scripts
    This was added to fix the "System running to use fully qualified
    hostnames" error while running the limerickctl script

--- start-dev.sh
+++ start-dev.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 cd `dirname $0`
-erl -pa $PWD/ebin $PWD/deps/*/ebin -name limerick@`hostname` -boot start_sasl -s limerick
+erl -pa $PWD/ebin $PWD/deps/*/ebin -sname limerick@`hostname` -boot start_sasl -s limerick
